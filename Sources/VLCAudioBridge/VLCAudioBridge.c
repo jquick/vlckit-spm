64887829276ac6692fc50481ed1dedebb07297c7
Jared Quick <jared.quick@MacBook-Pro.local>
2025-10-29 08:38:16 -0400

    add audio source file
    Signed-off-by: Jared Quick <jared.quick@MacBook-Pro.local>

---
+++ Sources/VLCAudioBridge/VLCAudioBridge.c
@@ -0,0 +1,13 @@
+//
+//  VLCAudioBridge.c
+//  VLCKitSPM
+//
+//  Stub implementation file required by Swift Package Manager.
+//  The actual functionality is declared in VLCAudioBridge.h as extern functions
+//  that are provided by libvlc (linked via VLCKit).
+//
+
+#include "VLCAudioBridge.h"
+
+// No implementation needed - libvlc_audio_set_callbacks is provided by libvlc
+
